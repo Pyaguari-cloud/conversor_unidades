ab11d66dd3de075ff080613440e961c82d87a96b
Pablo Yaguar <warspablo@gmail.com>
2026-02-17 15:02:10 -0500

    Segundo commit

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("conversor/", include("conversor.urls")),
+    path('', include('conversor.urls')),  # ← Opcional: redirigir raíz
+
 ]
